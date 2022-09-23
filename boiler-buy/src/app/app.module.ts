bd35277a5b273f7ef393377d5810b76781f0cf1f
reeves34 <reeves34@purdue.edu>
2022-09-23 01:05:38 -0400

    installed angular material

--- boiler-buy/src/app/app.module.ts
+++ boiler-buy/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProductSearchComponent } from './product-search/product-search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProductSearchComponent } from './product-search/product-search.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
