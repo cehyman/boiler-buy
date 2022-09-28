14944ecf58f8e2f81348aebc3aa9c68342410536
Christopher Hyman <chyman@purdue.edu>
2022-09-27 21:02:35 -0400

    basic api framework

--- listing/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ListingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'listing'
--- listing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>/', views.details, name='details'),
-    path('create', views.create_listing, name='create_listing'),
-    path('add', views.add_listing, name='add_listing'),
-]
