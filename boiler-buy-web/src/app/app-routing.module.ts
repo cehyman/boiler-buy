c8f6711ee1b603305e9299cd6e48cf4b65450bff
Michelle Song <micsongvcs@gmail.com>
2022-09-28 01:02:38 -0400

    profile component

--- boiler-buy-web/src/app/app-routing.module.ts
+++ boiler-buy-web/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
