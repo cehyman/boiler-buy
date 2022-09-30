53cc0f73bf636060a514e80a248d65c9115da350
Sanjith07 <60627851+Sanjith07@users.noreply.github.com>
2022-09-30 15:27:44 -0400

    Created update password service

---
+++ boiler-buy-web/src/app/change-password.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChangePasswordService {
+
+  updatePassword(userName:string, oldPassword:string, newPassword:string) {
+    
+  }
+
+  constructor() { }
+}
