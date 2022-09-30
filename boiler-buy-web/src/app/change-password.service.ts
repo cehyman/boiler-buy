599c47b77bcdb072ad749bf340e6852c52c0b36c
Sanjith07 <60627851+Sanjith07@users.noreply.github.com>
2022-09-30 15:35:48 -0400

    Updated change password service

--- boiler-buy-web/src/app/change-password.service.ts
+++ boiler-buy-web/src/app/change-password.service.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,8 +10,12 @@ import { Injectable } from '@angular/core';
 export class ChangePasswordService {
 
   updatePassword(userName:string, oldPassword:string, newPassword:string) {
-    
+    return this.http.post('http://localhost:8000/api/v1/updatePassword')
   }
 
-  constructor() { }
+  // getProductList(): Observable<ProductList> {
+  //   return this.http.get('http://localhost:8000/api/v1/products/', {responseType: 'json'}) as Observable<ProductList>;
+  // }
+
+  constructor(private http:HttpClient) { }
 }
