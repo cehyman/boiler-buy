d116bac7e60b3856db9d6eab69a563eaf6020f35
Joseph Chen <chen3242@purdue.edu>
2022-09-29 21:16:00 -0400

    Register UI, WIP on account backend

---
+++ boiler-buy-web/src/app/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'register.component',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+}
