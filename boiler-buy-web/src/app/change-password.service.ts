3a0ef1e751d27d5d290bdfbd45ee76b4b26caef6
Sanjith07 <60627851+Sanjith07@users.noreply.github.com>
2022-10-02 16:20:58 -0400

    Revert "Updated change password service"
    This reverts commit 1e79787ef8afa244e6f565a63255bd0172f2f2c1.

--- boiler-buy-web/src/app/change-password.service.ts
+++ boiler-buy-web/src/app/change-password.service.ts
@@ -1,8 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-import { Observable } from 'rxjs';
-
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +6,8 @@ import { Observable } from 'rxjs';
 export class ChangePasswordService {
 
   updatePassword(userName:string, oldPassword:string, newPassword:string) {
-    return this.http.post('http://localhost:8000/api/v1/updatePassword')
+    
   }
 
-  // getProductList(): Observable<ProductList> {
-  //   return this.http.get('http://localhost:8000/api/v1/products/', {responseType: 'json'}) as Observable<ProductList>;
-  // }
-
-  constructor(private http:HttpClient) { }
+  constructor() { }
 }
