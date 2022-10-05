3422247c566f5044b2e844e7723a05a9b7b36477
Richard Stump <rnstump@purdue.edu>
2022-10-05 17:32:43 -0400

    Add image preview

--- boiler-buy-web/src/app/picture-upload/picture-upload.component.ts
+++ boiler-buy-web/src/app/picture-upload/picture-upload.component.ts
@@ -8,13 +8,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PictureUploadComponent implements OnInit {
   selectedFiles: File[] = [];
-
+  previewUrls: any[] = [];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  // Adds the selected files from the file input into the selected file list
+  // This automatically converts the FileList object from the input element
+  // into a list of File objects for convienence. The files are not converted
   addFiles(event: Event) {
     var target: HTMLInputElement | null = event.target as HTMLInputElement;
 
@@ -25,6 +28,12 @@ export class PictureUploadComponent implements OnInit {
     if(files === null)
       return;
 
+    // Reader for converting the file into an image preview
+    var reader = new FileReader();
+    reader.onloadend = (event: any) => {
+      this.previewUrls.push(event.target.result);
+    }
+
     for (var i = 0; i < files.length; i++) {
       var file = files.item(i);
       if(file === null)
@@ -32,7 +41,11 @@ export class PictureUploadComponent implements OnInit {
 
       console.log(`Adding file ${file.name}`);
 
+      // Add the file object to a list so we have the name and oter info.
       this.selectedFiles.push(file);
+
+      // Convert the file of the image into a preview
+      reader.readAsDataURL(file);
     }
   }
 
