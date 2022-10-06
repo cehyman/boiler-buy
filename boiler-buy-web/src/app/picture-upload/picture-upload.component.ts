e1f425ee4f1b1284a97b1646278145a94be4c3f8
Richard Stump <rnstump@purdue.edu>
2022-10-05 20:47:33 -0400

    Add the ability for users to cancel a photo they have selected

--- boiler-buy-web/src/app/picture-upload/picture-upload.component.ts
+++ boiler-buy-web/src/app/picture-upload/picture-upload.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-picture-upload',
@@ -28,12 +29,6 @@ export class PictureUploadComponent implements OnInit {
     if(files === null)
       return;
 
-    // Reader for converting the file into an image preview
-    var reader = new FileReader();
-    reader.onloadend = (event: any) => {
-      this.previewUrls.push(event.target.result);
-    }
-
     for (var i = 0; i < files.length; i++) {
       var file = files.item(i);
       if(file === null)
@@ -45,13 +40,30 @@ export class PictureUploadComponent implements OnInit {
       this.selectedFiles.push(file);
 
       // Convert the file of the image into a preview
+
+      // Reader for converting the file into an image preview. This needs to be
+      // local to this loop rather than being created early because it takes 
+      // time for the file to be converted, and trying to convert one on the 
+      // same reader will cause issues if we didn't wait for it to finish.
+      var reader = new FileReader();
+      reader.onloadend = (event: any) => {
+        this.previewUrls.push(event.target.result);
+      }
       reader.readAsDataURL(file);
     }
   }
 
-  removePhoto(event: Event, name: String) {
-    console.log(`removing ${name}`);
+  removePhoto(event: Event) {
+    var target = event.target as HTMLButtonElement;
 
-    this.selectedFiles = this.selectedFiles.filter(item => name !== item.name);
+    if(target == null)
+      return;
+
+    // Grab the index of the button from its custom data attribute
+    var idx = Number(target.dataset['idx']);
+    console.log(`removing ${idx}`);
+
+    this.selectedFiles.splice(idx, 1);
+    this.previewUrls.splice(idx, 1);
   }
 }
