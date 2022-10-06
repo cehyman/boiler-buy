6a2da9bdccbe121050ae5fb30148d695815f5cdc
Richard Stump <rnstump@purdue.edu>
2022-10-06 02:37:01 -0400

    Remove the old create-listing component

--- boiler_buy_api/app/models.py
+++ boiler_buy_api/app/models.py
@@ -8,12 +8,6 @@ class Listing(models.Model):
     description = models.CharField(max_length=250)
     stock = models.IntegerField(default=1)
     
-    pictures = ArrayField(
-        models.ImageField(),
-        blank=True
-        null=True
-    )
-    
     def __str__(self):
         return self.name
         
