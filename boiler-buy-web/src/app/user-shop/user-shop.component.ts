c29cae23b3784476c0be18603f1d3fd37b88e80c
Richard Stump <54607143+Richard-Stump@users.noreply.github.com>
2022-11-12 13:18:04 -0500

    Update boiler-buy-web/src/app/user-shop/user-shop.component.ts
    Co-authored-by: reeves34 <54685028+reeves34@users.noreply.github.com>

--- boiler-buy-web/src/app/user-shop/user-shop.component.ts
+++ boiler-buy-web/src/app/user-shop/user-shop.component.ts
@@ -33,7 +33,7 @@ export class UserShopComponent implements OnInit {
     this.curruser = <string> this.appcomp.getUsername()
 
     var urlStr = this.activatedRoute.snapshot.url.toString();
-    console.log(urlStr);
+    console.log('urlStr: ', urlStr);
     this.shop_id = Number(urlStr.split(',')[1]);
 
     if(isNaN(this.shop_id)) {
