cdef72b4e1d2e8e2282f213affef3a9c6adcd9c8
Michelle Song <micsongvcs@gmail.com>
2022-12-09 11:25:16 -0500

    added block button to settings in chat ui and begin adding functionality

--- boiler-buy-web/src/app/general-chat/general-chat.component.ts
+++ boiler-buy-web/src/app/general-chat/general-chat.component.ts
@@ -17,6 +17,11 @@ export class GeneralChatComponent implements OnInit {
   public globals: Globals = new Globals;
   private appcomp: AppComponent = new AppComponent();
 
+  //blocked users vars
+  blockedList: string[] = []
+  blockedListOtherEmail: string[] = []
+  isBlocked: boolean = false
+
   public chatInfo: ChatGroup = {currEmail: "", otherEmail: "", productID: 0} as ChatGroup;
 
   messages: {name:string, message:string, date:string, image:string}[] = [];
@@ -50,6 +55,8 @@ export class GeneralChatComponent implements OnInit {
 
     var urlParams = new URLSearchParams();
     urlParams.set('id', "" + this.id)
+
+    this.checkBlock()
 
     this.chatService.getMessages({
       currEmail: this.appcomp.getEmail(),
@@ -118,20 +125,25 @@ export class GeneralChatComponent implements OnInit {
     if (message != "") {
       console.log("clicked!", message);
 
-      this.chatService.sendMessage({
-        senderEmail: this.chatInfo.currEmail,
-        receiverEmail: this.chatInfo.otherEmail,
-        productID: this.chatInfo.productID,
-        message: message
-      } as ChatMessageItem).subscribe((success) => {
+      if (this.isBlocked) {
+        console.log("blocked from sending a message")
+        alert("Unable to send message")
+      } else {
+        this.chatService.sendMessage({
+          senderEmail: this.chatInfo.currEmail,
+          receiverEmail: this.chatInfo.otherEmail,
+          productID: this.chatInfo.productID,
+          message: message
+        } as ChatMessageItem).subscribe((success) => {
 
-        this.messages = this.messages.concat([{"name": this.chatInfo.currEmail, "message": message, "date":"now", image: this.currImage}]);
-        window.scroll(0, document.documentElement.offsetHeight);
-        console.log(success)
-      }, (error) => {
-        console.log(error)
-        alert("There was an issue with sending your message. Please try again")
-      })
+          this.messages = this.messages.concat([{"name": this.chatInfo.currEmail, "message": message, "date":"now", image: this.currImage}]);
+          window.scroll(0, document.documentElement.offsetHeight);
+          console.log(success)
+        }, (error) => {
+          console.log(error)
+          alert("There was an issue with sending your message. Please try again")
+        })
+      }
     }
   }
 
@@ -160,6 +172,108 @@ export class GeneralChatComponent implements OnInit {
 
       this.http.get('api/chatGroup/?' + params.toString(), {responseType: 'json'}).subscribe()
 
+    })
+  }
+
+  checkBlock() {
+    var request = this.http.get<any>('api/accounts/'+this.chatInfo.currEmail+'/', {observe: "body"});
+    request.subscribe((data: any) => {
+      console.log(data)
+      let checkBlockList = data.blockedUsers
+      console.log(checkBlockList)
+
+      //check if user is already in blocked list
+      var inList = false;
+      for(let i = 0; i < this.blockedList.length; i++) {
+        if (this.blockedList[i] === this.chatInfo.otherEmail) {
+          inList = true;
+          break;
+        }
+      }
+
+      if(inList) {
+        alert("user blocked")
+        this.isBlocked = true
+      }
+    })
+  }
+
+  blockUser() {
+    //add to block column in account db to both buyer and seller 
+
+    //request to backend to add to blocked users
+    //get the blocked users list
+    var request = this.http.get<any>('api/accounts/'+this.chatInfo.currEmail+'/', {observe: "body"});
+    request.subscribe((data: any) => {
+      console.log(data)
+      this.blockedList = data.blockedUsers
+      console.log(this.blockedList)
+
+      console.log("trying to block " + this.chatInfo.otherEmail)
+
+      //check if user is already in blocked list
+      var inList = false;
+      for(let i = 0; i < this.blockedList.length; i++) {
+        if (this.blockedList[i] === this.chatInfo.otherEmail) {
+          inList = true;
+          break;
+        }
+      }
+
+      if(inList) {
+        alert("already blocked")
+        return;
+      }
+
+      this.blockedList.push(<string>this.chatInfo.otherEmail)
+
+      var body = {
+        blockedUsers: this.blockedList
+      }
+
+      //send another request to update the current users block list
+      var request2 = this.http.patch<any>(`http://localhost:8000/api/accounts/`+this.chatInfo.currEmail+'/', body, {observe: "response"});
+      request2.subscribe((data: any) => {
+        console.log("Added " + this.chatInfo.otherEmail + " to Blocked List")
+        this.isBlocked = true
+      })
+    })
+
+    //add to other emails blocked list
+    var request3 = this.http.get<any>('api/accounts/'+this.chatInfo.otherEmail+'/', {observe: "body"});
+    request3.subscribe((data: any) => {
+      console.log(data)
+      this.blockedListOtherEmail = data.blockedUsers
+      console.log(this.blockedList)
+
+      console.log("trying to block " + this.chatInfo.currEmail)
+
+      //check if user is already in blocked list
+      var inList = false;
+      for(let i = 0; i < this.blockedListOtherEmail.length; i++) {
+        if (this.blockedListOtherEmail[i] === this.chatInfo.currEmail) {
+          inList = true;
+          break;
+        }
+      }
+
+      if(inList) {
+        alert("already blocked")
+        return;
+      }
+
+      this.blockedListOtherEmail.push(<string>this.chatInfo.currEmail)
+
+      var body = {
+        blockedUsers: this.blockedListOtherEmail
+      }
+
+      //send another request to update the current users block list
+      var request4 = this.http.patch<any>(`http://localhost:8000/api/accounts/`+this.chatInfo.otherEmail+'/', body, {observe: "response"});
+      request4.subscribe((data: any) => {
+        console.log("Added " + this.chatInfo.currEmail + " to Blocked List")
+        this.isBlocked = true
+      })
     })
   }
 }
